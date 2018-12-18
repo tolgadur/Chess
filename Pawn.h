511af7a4bfbee63bc45f083d0e8906cdbcda4188
Tolga Dur <durtolga1@gmail.com>
2018-12-18 22:53:09 +0000

    Create Pawn.h

---
+++ Pawn.h
@@ -0,0 +1,14 @@
+#ifndef PAWN_H
+#define PAWN_H
+
+#include "Piece.h"
+
+class Pawn : public Piece{
+private:
+public:
+  Pawn(bool is_white) : Piece(is_white, "Pawn"){ };
+  ~Pawn() override{ };
+  bool isMoveOK(int s_row, int s_column, int d_row, int d_column, Piece* board[8][8]) override;
+};
+
+#endif
