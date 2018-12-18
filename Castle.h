b90c9f4caaa79ea57d0be824f10f2b135699cdab
Tolga Dur <durtolga1@gmail.com>
2018-12-18 22:47:07 +0000

    Create Castle.h

---
+++ Castle.h
@@ -0,0 +1,15 @@
+#ifndef CASTLE_H
+#define CASTLE_H
+
+#include "Piece.h"
+
+
+class Castle : public Piece{
+private:
+public:
+  Castle(bool is_white) : Piece(is_white, "Castle"){ };
+  ~Castle() override{ };
+  bool isMoveOK(int s_row, int s_column, int d_row, int d_column, Piece* board[8][8]) override;
+};
+
+#endif
