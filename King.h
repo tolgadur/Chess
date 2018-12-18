f7d68f0ef3b19128147ffa4cee53fd707abfd584
Tolga Dur <durtolga1@gmail.com>
2018-12-18 22:51:58 +0000

    Create King.h

---
+++ King.h
@@ -0,0 +1,14 @@
+#ifndef KING_H
+#define KING_H
+
+#include "Piece.h"
+
+class King : public Piece{
+private:
+public:
+  King(bool is_white) : Piece(is_white, "King"){ };
+  ~King() override{ };
+  bool isMoveOK(int s_row, int s_column, int d_row, int d_column, Piece* board[8][8]) override;
+};
+
+#endif
